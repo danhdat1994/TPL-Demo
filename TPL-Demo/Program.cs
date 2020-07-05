bd8b325ce9de6bfcdf532e727e942a033d4c65e3
Nguyen Bui Danh Dat <danhdat1994@gmail.com>
2020-07-05 15:33:10 +0700

    Add project files.

---
+++ TPL-Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace TPL_Demo
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Task 1 starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 ending");
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Task 2 starting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 ending");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
