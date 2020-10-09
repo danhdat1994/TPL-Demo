eceac82c965a3920824effb2879e4c5aa6aa0ccb
dat.nguyen <dat.nguyen@HCM-DATNBD>
2020-10-09 08:16:05 +0700

    loop

--- TPL-Demo/Program.cs
+++ TPL-Demo/Program.cs
@@ -19,11 +19,47 @@ namespace TPL_Demo
             Thread.Sleep(1000);
             Console.WriteLine("Task 2 ending");
         }
+
+        static void ParallelInvolke()
+        {
+            Parallel.Invoke(() => Task1(), () => Task2());
+        }
+
         static void WorkOnItem(object item)
         {
             Console.WriteLine("Started working on: " + item);
             Thread.Sleep(100);
             Console.WriteLine("Finished working on: " + item);
+        }
+
+        static void ParallelForEach()
+        {
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item =>
+            {
+                WorkOnItem(item);
+            });
+        }
+
+        static void ParallelFor()
+        {
+            var items = Enumerable.Range(0, 500).ToArray();
+            Parallel.For(0, items.Length, i =>
+            {
+                WorkOnItem(items[i]);
+            });
+        }
+
+        static void LoopState()
+        {
+            var items = Enumerable.Range(0, 500).ToArray();
+            ParallelLoopResult result = Parallel.For(0, items.Count(), (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 200)
+                    loopState.Stop();
+
+                WorkOnItem(items[i]);
+            });
         }
 
         static void Main(string[] args)
